183239e0ccb213d3ee3590efc20654472281041a
Josh Pigford <josh@joshpigford.com>
2022-11-29 17:18:51 -0600

    Investments page

--- components/layout.js
+++ components/layout.js
@@ -3,43 +3,46 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export const siteTitle = 'Josh Pigford';
+export default function Layout(props) {
+  const pageTitle = props.title ? `${props.title} - Josh Pigford` : 'Josh Pigford';
+  const pageDesc = props.desc ? props.desc : 'Josh Pigford: Maker. Dabbler. Founder and CEO of Maybe.';
 
-export default function Layout({ children, home }) {
   return (
     <>
       <Head>
-        <title>{siteTitle}</title>
+        <title>{pageTitle}</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <meta name="title" content={siteTitle} />
-        <meta name="description" content={siteTitle} />
+        <meta name="title" content={pageTitle} />
+        <meta name="description" content={pageDesc} />
 
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
-        <meta property="og:title" content={siteTitle} />
-        <meta property="og:description" content={siteTitle} />
+        <meta property="og:title" content={pageTitle} />
+        <meta property="og:description" content={pageDesc} />
         <meta property="og:image" content="" />
 
         {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:title" content={siteTitle} />
-        <meta property="twitter:description" content={siteTitle} />
+        <meta property="twitter:title" content={pageTitle} />
+        <meta property="twitter:description" content={pageDesc} />
         <meta property="twitter:image" content="" />
       </Head>
       <header className="flex mb-10">
         <div className="flex-none mr-8">
-          <Image
-            priority
-            src="/images/joshpigford-avatar.png"
-            height={144}
-            width={144}
-            alt="Josh Pigford"
-            className="w-36 h-36 rounded-full"
-          />
+          <Link href="/">
+            <Image
+              priority
+              src="/images/joshpigford-avatar.png"
+              height={144}
+              width={144}
+              alt="Josh Pigford"
+              className="w-36 h-36 rounded-full"
+            />
+          </Link>
         </div>
         <div className="flex-auto mt-3">
-          <h1 className="mb-3 text-3xl font-black inline">{siteTitle}</h1>
+          <h1 className="mb-3 text-3xl font-black inline"><Link href="/" className="text-black">Josh Pigford</Link></h1>
           <div className='inline ml-4 relative -top-1.5'>
             <a href="https://twitter.com/Shpigford?ref_src=twsrc%5Etfw" className="twitter-follow-button" data-size="large" data-dnt="true" data-show-count="true">Follow @Shpigford</a>
             <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
@@ -51,7 +54,7 @@ export default function Layout({ children, home }) {
         </div>
       </header>
 
-      {children}
+      {props.children}
 
       <footer className="mt-20 text-sm text-gray-400">
         This is a footer and I have no idea what to put here. <a href="https://github.com/Shpigford/joshpigford.com/commits/main">Changelog</a>.
---
+++ pages/investments.js
@@ -0,0 +1,107 @@
+/* eslint-disable react/no-unescaped-entities */
+import Layout from '../components/layout'
+import Head from 'next/head';
+
+export default function Investments() {
+  return (
+    <Layout title="Investments" desc="I've been doing tiny investments in startups since 2021, primarily to just get my feet wet and to diversify my high-level personal finance holdings. Here are the investments I've made.">
+      <Head>
+        <title>Investments</title>
+      </Head>
+      <div className="prose max-w-none">
+        <h1>Investments</h1>
+        <p>I've been doing tiny investments in startups since 2021, primarily to just get my feet wet and to diversify my high-level personal finance holdings. Here are the investments I've made.</p>
+        <table>
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Year</th>
+              <th>Amount</th>
+              <th>About</th>
+            </tr>
+          </thead>
+          <tbody className="text-sm">
+            <tr className="odd:bg-white even:bg-gray-100">
+              <td className="px-2 py-3"><a href="https://reflect.app">Reflect</a></td>
+              <td>November 2022</td>
+              <td>$1,000</td>
+              <td>Thought organization tool</td>
+            </tr>
+            <tr className="odd:bg-white even:bg-gray-100">
+              <td className="px-2 py-3"><a href="https://anydistance.club">AnyDistance</a></td>
+              <td>March 2022</td>
+              <td>$10,000</td>
+              <td>Interactive health company + community</td>
+            </tr>
+            <tr className="odd:bg-white even:bg-gray-100">
+              <td className="px-2 py-3"><a href="https://www.xiggit.com">Xiggit</a></td>
+              <td>March 2022</td>
+              <td>$1,000</td>
+              <td>Benefits for hourly workers and freelancers</td>
+            </tr>
+            <tr className="odd:bg-white even:bg-gray-100">
+              <td className="px-2 py-3"><a href="https://www.ooneepod.com">Oonee</a></td>
+              <td>March 2022</td>
+              <td>$1,000</td>
+              <td>Smart, modular, scalable bike parking</td>
+            </tr>
+            <tr className="odd:bg-white even:bg-gray-100">
+              <td className="px-2 py-3"><a href="https://thimble.io">Thimble</a></td>
+              <td>March 2022</td>
+              <td>$1,000</td>
+              <td>Live STEM classes + robotics kits for schools</td>
+            </tr>
+            <tr className="odd:bg-white even:bg-gray-100">
+              <td className="px-2 py-3"><a href="https://bowr.co">Bowr</a></td>
+              <td>March 2022</td>
+              <td>$1,000</td>
+              <td>Sustainable furniture design & production</td>
+            </tr>
+            <tr className="odd:bg-white even:bg-gray-100">
+              <td className="px-2 py-3"><a href="https://kingscrowd.com">KingsCrowd</a></td>
+              <td>March 2022</td>
+              <td>$1,000</td>
+              <td>Financial data platform for online private markets</td>
+            </tr>
+            <tr className="odd:bg-white even:bg-gray-100">
+              <td className="px-2 py-3"><a href="https://brikclik.com">Brik + Clik</a></td>
+              <td>March 2022</td>
+              <td>$1,000</td>
+              <td>End-to-end commerce solution for brands and retailers</td>
+            </tr>
+            <tr className="odd:bg-white even:bg-gray-100">
+              <td className="px-2 py-3"><a href="https://curastory.co">Curastory</a></td>
+              <td>February 2022</td>
+              <td>$1,000</td>
+              <td>Creator platform for making, monetizing and distributing video content</td>
+            </tr>
+            <tr className="odd:bg-white even:bg-gray-100">
+              <td className="px-2 py-3"><a href="https://nada.co/cityfunds/33100">Miami Cityfund</a></td>
+              <td>February 2022</td>
+              <td>$1,000</td>
+              <td>Real estate index fund of Miami</td>
+            </tr>
+            <tr className="odd:bg-white even:bg-gray-100">
+              <td className="px-2 py-3"><a href="https://www.evolutiondevices.com">Evolution Devices</a></td>
+              <td>January 2022</td>
+              <td>$1,000</td>
+              <td>Digital physical therapy with smart therapeutic devices</td>
+            </tr>
+            <tr className="odd:bg-white even:bg-gray-100">
+              <td className="px-2 py-3"><a href="https://customer.io">Customer.io</a></td>
+              <td>August 2021</td>
+              <td>$2,500</td>
+              <td>Business messaging automation platform</td>
+            </tr>
+            <tr className="odd:bg-white even:bg-gray-100">
+              <td className="px-2 py-3"><a href="https://gumroad.com">Gumroad</a></td>
+              <td>April 2021</td>
+              <td>$1,000</td>
+              <td>Storefront for creators</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </Layout>
+  )
+}
